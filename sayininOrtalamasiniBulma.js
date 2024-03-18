c38d3c0c59a3e6d362eeeddc4d97f442ad7faaf1
grafikeh <58554344+grafikeh@users.noreply.github.com>
2024-03-18 13:49:39 +0300

    Update and rename sayininOrtalamasiniBulma to sayininOrtalamasiniBulma.js

--- sayininOrtalamasiniBulma.js
+++ sayininOrtalamasiniBulma.js
@@ -1,5 +1,5 @@
-Klavyeden girilen 3 sayının ortalamasını alan bir uygulama yazınız.
---------------------------------------------------------------------
+/*Klavyeden girilen 3 sayının ortalamasını alan bir uygulama yazınız.
+--------------------------------------------------------------------*/
 let birinciSayi = parseInt(prompt("Birinci sayiyi giriniz", "1")); //parseInt diyerek çift tırnak içerisindeki kullanıcının girdiği değer metinden sayıya dönüştürülecek
 let ikinciSayi = parseInt(prompt("İkinci sayiyi giriniz", "1"));
 let ucuncuSayi = parseInt(prompt("Üçüncü sayiyi giriniz", "1"));
