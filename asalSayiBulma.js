85747920ff6e31299ab34e0f5aaaa2e3b24d19ea
grafikeh <58554344+grafikeh@users.noreply.github.com>
2024-03-18 13:44:04 +0300

    Update and rename asalSayiBulma to asalSayiBulma.js

--- asalSayiBulma.js
+++ asalSayiBulma.js
@@ -1,4 +1,4 @@
-Kullanıcıdan aldığınız integer değere kadar olan tümasal sayıları yazdıran  programı yazınız. 10 için ekranda 2,3,5,7  değerleri olmalıdır.
+//Kullanıcıdan aldığınız integer değere kadar olan tümasal sayıları yazdıran  programı yazınız. 10 için ekranda 2,3,5,7  değerleri olmalıdır.
 -----------------------------------------------------------
 let girilenSayi = parseInt(prompt("Bir sayı giriniz","10"));
 let gosterilecekMetin = "";
