12d51bbaa5af3e7c996d436dc56de2bfa1228e67
grafikeh <58554344+grafikeh@users.noreply.github.com>
2024-03-18 13:46:21 +0300

    Update artıkYil.js

--- artıkYil.js
+++ artıkYil.js
@@ -2,7 +2,7 @@
 Bir yılın artık olması için ya 400 ile modu 0 olmalı
 ya da 4 ile modu 0 olmalı ve 100 ile modu 0 olmalı
 4100 değeri false değeri vermeli */
----------------------------------------------------------
+//---------------------------------------------------------
 let girilenYil = parseInt (prompt("Yıl değerini giriniz", "2020"));
 
 let artikYilMi = ((girilenYil % 400)==0) || (girilenYil %4 == 0 && girilenYil %100 != 0);
