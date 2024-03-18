08fe24e1160618c517b729ea5ec57fae63a72147
grafikeh <58554344+grafikeh@users.noreply.github.com>
2024-03-18 13:47:14 +0300

    Update and rename faktoriyel to faktoriyel.js

--- faktoriyel.js
+++ faktoriyel.js
@@ -1,5 +1,5 @@
-Klavyeden girilen bir sayının faktöriyelini alan bir uygulama yazınız.
-------------------------------------------------------------------------
+/*Klavyeden girilen bir sayının faktöriyelini alan bir uygulama yazınız.
+------------------------------------------------------------------------*/
 let sayi = parseInt(prompt("Faktöriyeli hesaplanacak bir sayı giriniz", "10"));
 let faktoriyel = 1;
 for(let i=1; i<=sayi; i++){
