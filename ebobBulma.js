a18de6cec0cabcf7986cf72145572ac44eb96f32
grafikeh <58554344+grafikeh@users.noreply.github.com>
2024-03-18 13:45:22 +0300

    Update ebobBulma.js

--- ebobBulma.js
+++ ebobBulma.js
@@ -1,5 +1,5 @@
 //Kullanıcıdan aldığınız iki tamsayı değerin en büyük ortak bölenini uygulamayı yazınız. Örneğin 14 ve 35 sayıları için ebob = 7 olmalıdır.
-//----------------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------
 let sayi1 = parseInt(prompt("Birinci sayıyı giriniz"));
 let sayi2 = parseInt(prompt("İkinci sayıyı giriniz"));
 
