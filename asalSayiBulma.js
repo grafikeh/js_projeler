35377e5cab74197b9b7a699265b26e823f080dfa
grafikeh <58554344+grafikeh@users.noreply.github.com>
2024-03-18 13:46:34 +0300

    Update asalSayiBulma.js

--- asalSayiBulma.js
+++ asalSayiBulma.js
@@ -1,5 +1,5 @@
 //Kullanıcıdan aldığınız integer değere kadar olan tümasal sayıları yazdıran  programı yazınız. 10 için ekranda 2,3,5,7  değerleri olmalıdır.
------------------------------------------------------------
+//-----------------------------------------------------------
 let girilenSayi = parseInt(prompt("Bir sayı giriniz","10"));
 let gosterilecekMetin = "";
 for(let i=2; i<= girilenSayi; i++){
