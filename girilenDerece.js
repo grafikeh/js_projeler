2414bcff70eabc0a080b00a9e6c5c0113c3ee343
grafikeh <58554344+grafikeh@users.noreply.github.com>
2024-03-18 13:47:46 +0300

    Update and rename girilenDerece to girilenDerece.js

--- girilenDerece.js
+++ girilenDerece.js
@@ -1,4 +1,4 @@
-Bir değişken oluşturun ve bu değişkende fahrenheit cinsinden sıcaklığı saklayın.
+/*Bir değişken oluşturun ve bu değişkende fahrenheit cinsinden sıcaklığı saklayın.
 Sonrasında bu değerin kaç Celcius olduğunu hesaplayın.
 100 değeri için 37.777778 gibi bir değer elde edilmektedir.
 Hesaplama formülü : celcius = 5/9*(fahrenheit -32) */
